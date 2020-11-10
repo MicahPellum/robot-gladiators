d837736c0cb56f61e27ff6e961716ff1ef706233
Micah Pellum <pellummicah@gmail.com>
2020-11-10 16:33:38 -0700

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,37 +9,54 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var promptFight= window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose");
+var playerMoney= 10;
+
 var fight = function() {
     // alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
-    
-  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-  enemyHealth = enemyHealth - playerAttack;
+    // if a player choose to fight, then fight
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        // remove enemy's health by subtracting the amount set in the playerAttack variable
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
+            playerName + " attacked " + enemyName + "." + enemyName + " now has " + enemyHealth + " health remaining."
+        );
 
-  // Log a resulting message to the console so we know that it worked.
-  console.log (
-      playerName + " attacked " + enemyName + "." + enemyName + " now has " + enemyHealth + " health remaining."
-  );
+
   // check enemy's health
   if (enemyHealth <=0) {
     window.alert(enemyName + " has died!");
-}
-else {
+} else {
     window.alert(enemyName + " still has " + enemyHealth + " health left.");
 }
-};
-
-  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+// remove player's health by subtractiong the amount set in the enemyAttack variable
 playerHealth = playerHealth - enemyAttack;
-  // Log a resulting message to the console so we know that it worked.
-  console.log(
-enemyName + " attacked " + playerName + "." + playerName + " now has " + playerHealth + " health remaining.");
-//check players health
-if (playerHealth <= 0) {
-    window.alert(playerName + " has died!");
-}
-else {
-    window.alert(playerName + " still has " + playerHealth + " health left.");
-}
+console.log(
+enemyName + " attacked" + playerName + "." + playerName + " now has " + playerHealth + " health remaining."
+);
 
+// check players health
+if (playerHealth <= 0) {
+    window.alert(player + " has died!");
+} else {
+    window.alert(playerName + "still has " + playerHealth + " health left.");
+} 
+  // if player choses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    //confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    //if yes (true), leave fight
+    if (confirmSkip) {
+    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+    // subtract money from playerMoney for skipping
+    playerMoney = playerMoney - 2;
+    } 
+// if no (false), ask question again by running fight () again
+else {
+    fight ();
+    }
+}
+}
 fight ();

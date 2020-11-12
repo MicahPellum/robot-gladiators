733c483c3a3fc835c6950b6bfe2837c301bbf06a
Micah Pellum <pellummicah@gmail.com>
2020-11-12 09:41:37 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,21 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
-
+var playerMoney = 10;
 //logging multiple values at once
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+console.log(playerName, playerAttack, playerHealth, playerMoney);
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 var promptFight= window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose");
-var playerMoney= 10;
 
-var fight = function() {
+var fight = function(enemyName) {
+    for(var i =0; i < enemyNames.length; i++) {
+        console.log(enemyNames[i]);
+        console.log(i);
+        console.log(enemyNames[i] + " is at " + i + " index");
+    }
     // alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     // if a player choose to fight, then fight
@@ -40,7 +43,7 @@ enemyName + " attacked" + playerName + "." + playerName + " now has " + playerHe
 if (playerHealth <= 0) {
     window.alert(player + " has died!");
 } else {
-    window.alert(playerName + "still has " + playerHealth + " health left.");
+    window.alert(playerName + " still has " + playerHealth + " health left.");
 } 
   // if player choses to skip
 } else if (promptFight === "skip" || promptFight === "SKIP") {
@@ -57,6 +60,17 @@ if (playerHealth <= 0) {
 else {
     fight ();
     }
+
 }
 }
-fight ();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
+
+//fight ();
+
+// Game States//
+// "WIN" - Player robot has defeated all enemy-robots
+//  *  Fight all enemy-robots
+//  * Defeat each enemy-robot
+// "LOSE" -  Player robot's health is zero or less
